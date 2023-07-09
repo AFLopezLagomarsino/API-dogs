b63dc2d71f31ca08dbd2fef9ced964fdf38f87ce
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-07-08 22:10:15 -0300

    front configurado

---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+
+function landingPage () {
+
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <button>Home</button>
+        </div>
+    )
+
+}
+
+export default landingPage
