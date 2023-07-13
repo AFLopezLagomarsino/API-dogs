1afaf0c8ba75c61c72138dda3d2881e056e0a1bb
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-07-12 21:13:18 -0300

    la barra de busqueda ahora es funcional, busca tanto por nombre exacto, como por coincidencias, pero hay un error al no encontrar ninguna coincidencia

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -1,17 +1,24 @@
 
 import {useState} from "react"
-
+import {getByBreed} from "../../Redux/actions/actions"
+import { useDispatch } from "react-redux"
 function SearchBar (){
-
     const [name, setName] = useState("")
-
+    const dispatch = useDispatch()
     function handleChange(e){
+        e.preventDefault()
         setName(e.target.value)
+    }
+    function handleSubmit(e){
+        e.preventDefault()
+        dispatch(getByBreed(name))
+        setName("")
     }
 
     return(
         <div>
-            <input type="search" onChange={handleChange} value={name} />       
+            <input type="search" onChange={handleChange} value={name} placeholder="search breed..." />       
+            <button type="submit" onClick={handleSubmit}>Search</button>
         </div>
     )
 
