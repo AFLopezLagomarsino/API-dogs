47b5f5f67fd9ebe39b611fdf45709c9be89f2422
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-07-12 21:09:08 -0300

    algunos bugs arreglados, se encontro uno que al no escribir una coincidencia en la barra de busqueda no entra en el control de errores

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,25 @@
 import { useState, useEffect } from "react";
 import {useDispatch, useSelector} from "react-redux"
-import { getDogs } from "../../Redux/actions.js/actions";
+import { getDogs } from "../../Redux/actions/actions";
 import Nav from "../Nav/nav";
 import Card from "../card/card";
+import Paginado from "../pagination/pagination";
 
 function Home () {
 
     const dispatch = useDispatch()
-    const dogs = useSelector(state => state.dogs)
+    const allDogs = useSelector(state => state.dogs)
+    //paginado owo
+    const [currentPage, setCurrentPage] = useState(1)
+    const [dogsPerPage, setDogsPerPage] = useState(8)
+    const indexOfLastDog = currentPage * dogsPerPage // 
+    const indexOfFirstDog = indexOfLastDog - dogsPerPage //
+    const currentDogs = allDogs.slice(indexOfFirstDog, indexOfLastDog)  
+    
+
+    const paginado = (pageNumber) =>{
+        setCurrentPage(pageNumber)
+    }
 
     useEffect(() => {
         dispatch(getDogs())
@@ -21,17 +33,17 @@ function Home () {
     return (
         <div>
             <h1>Home</h1>
-            <Nav />
-            <button onClick={e=>{handleClick(e)}}>
-            refresh doguitos
-            </button>
+            <Nav paginado={paginado} />
+            <button onClick={e=>{handleClick(e)}}>refresh doguitos</button>
+            <Paginado dogsPerPage={dogsPerPage} dogs={allDogs.length} paginado={paginado}/>
             <div>
                 {
-                    dogs && dogs.map(element=>{
-                        return <Card name={element.name} image={element.image} temperaments={element.temperaments} weightMin={element.weightMin} weightMax={element.weightMax}/>
+                    currentDogs && currentDogs.map(element=>{
+                        return <Card name={element.name} image={element.image} temperaments={element.temperaments} weightMin={element.weightMin} weightMax={element.weightMax} key ={element.id}/>
                     })
                 }
             </div>
+            <Paginado dogsPerPage={dogsPerPage} dogs={allDogs.length} paginado={paginado}/>
         </div>
 
     )
